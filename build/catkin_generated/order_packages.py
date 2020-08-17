395308730220a6dcdabce959aa4716795e30d0a3
HaibinZHAO <zhb950102@qq.com>
2020-08-18 02:04:58 +0300

    update

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/haibin/Desktop/软件/Learn-ROS/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/haibin/Desktop/软件/Learn-ROS/devel;/opt/ros/melodic'.split(';') if '/home/haibin/Desktop/软件/Learn-ROS/devel;/opt/ros/melodic' != '' else []
